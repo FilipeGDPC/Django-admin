fdcd50885a91c7e13d9c965461260444faa07e74
Filipe Gama <filipegama2411@gmail.com>
2024-01-23 20:47:27 -0300

    improvements on Html

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Olá, mundo!")
+    
+    context = {
+        "nome_pagina": "Início da dashboard",
+    }
+    
+    return render(request, "index.html", context)
+
+
