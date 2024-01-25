8b0fee45ec1072afeb0039f04dd5e065ae037e87
Filipe Gama <filipegama2411@gmail.com>
2024-01-25 13:03:36 -0300

    Optimazing messages in index.html and forms.html

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -9,3 +9,18 @@ class VisitanteForm(forms.ModelForm):
             "nome_completo", "cpf", "data_nascimento",
             "numero_casa", "placa_veiculo"
         ]
+        error_messages = {
+           "nome_completo": {
+                "required": "O nome completo do visitante é obrigatório para cadastro",
+            },
+            "cpf": {
+                "required": "O CPF do visitante é obrigatório para cadastro"
+            },
+            "data_nascimento": {
+                "required": "A data de nascimento do visitante é obrigatória para cadastro",
+                "invalid": "Por favor, informe um formato válido para a data de nascimento (DD/MM/AAAA)"
+            },
+            "numero_casa": {
+                "required": "Por favor, informe o número da casa a ser visitada"
+            },
+        }
