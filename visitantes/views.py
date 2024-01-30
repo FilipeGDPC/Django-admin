9492e84d1ee83e0ec66b9d1be428c203cec72abb
Filipe Gama <filipegama2411@gmail.com>
2024-01-30 08:19:52 -0300

    Creating function to end visit

--- visitantes/views.py
+++ visitantes/views.py
@@ -74,6 +74,24 @@ def informacoes_visitante(request, id):
     
     return render(request, "informacoes_visitante.html", context)
 
-
+def finalizar_visita(request, id):
+    
+    if request.method == "POST":
+        Visitante = get_object_or_404(
+            Visitante,
+            id=id
+        )
+        
+        Visitante.status = "FINALIZADO"
+        Visitante.horario_saida = timezone.now()
+        
+        Visitante.save()
+        
+        messages.success(
+          request,
+          "Visita finalizada com sucesso"
+        )
+        
+        return redirect("index")
 
 
