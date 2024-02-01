7ebabf5ef7a956412d5f7ce3e0c5f802c903bbbc
Filipe Gama <filipegama2411@gmail.com>
2024-02-01 13:14:45 -0300

    adjusting login from url

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path(
         "login/",
         auth_views.LoginView.as_view(
-            template_name = "login.html"
+            template_name="login.html"
         ),
         name="login"
     ),
