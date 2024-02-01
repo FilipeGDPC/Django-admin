c272aaa28bf20057d0cf68a4008da66c5debc900
Filipe Gama <filipegama2411@gmail.com>
2024-02-01 13:32:47 -0300

    creating the url for logout

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -19,6 +19,15 @@ urlpatterns = [
         ),
         name="login"
     ),
+    
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(
+            template_name="logout.html"
+        ),
+        name="logout"
+    ),
+    
 
     path(
         "",
